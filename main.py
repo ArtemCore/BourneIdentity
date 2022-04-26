ebc6883ddef26da3ab3e943f2513b22aeb942e7e
Artem Tsitsvira <artem.t@core-tech.ru>
2022-04-26 15:10:43 +0300

    Removed iterrows, fixed typo

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from typing import Any, Dict, List, Set
 
 import numpy as np
 import pandas as pd
-from pandas import DataFrame
 
 from reader import read_file_to_df
 
-currentPath = os.getcwd()
-chunk_size = 2
+CURRENT_PATH = os.getcwd()
+CHUNK_SIZE = 2  # number of rows to read
 
 
 def howTo():
@@ -21,30 +20,26 @@ def howTo():
 def run(args_to_process: List[str]):
     if args_to_process[0] in ["-h", "--help"]:
         return howTo()
-    data_frames_list: List[DataFrame] = []
+    data_frames_list: List[pd.DataFrame] = []
     for file in args_to_process:
-        file_path: str = f"{currentPath}/{file}"
-        df: DataFrame = read_file_to_df(file_path, chunksize=chunk_size)
+        file_path: str = f"{CURRENT_PATH}/{file}"
+        df: pd.DataFrame = read_file_to_df(file_path, chunksize=CHUNK_SIZE)
         data_frames_list.append(df)
-    should_print_jb: bool = process_df(data_frames_list)
-    if should_print_jb:
-        pass
-        # print_jb()
+    result: List[str] = process_df(data_frames_list)
+    for line in result:
+        print(line)
 
 
 def print_jb():
-    with open(f"{currentPath}/jb.txt", "r") as jb:
+    with open(f"{CURRENT_PATH}/jb.txt", "r") as jb:
         jb_lines_list = jb.readlines()
     for line in jb_lines_list:
         print(line.strip())
 
 
-def process_df(data_frames_list: List[DataFrame]) -> bool:
-    should_print_jb = False
+def process_df(data_frames_list: List[pd.DataFrame]) -> List[str]:
     # concat all dataframes in one
-    concatinated_df: DataFrame = pd.concat(data_frames_list).replace(
-        {np.nan: None}
-    )
+    concatenated_df: pd.DataFrame = pd.concat(data_frames_list).replace({np.nan: None})
     search_params: Dict[str, Set] = {"Full name": {"Jason Bourne",}}
     exclude_columns: List[str] = ["Time", "is_bourne", "file_name"]
     used_params: Dict[str, Set] = dict()
@@ -52,20 +47,18 @@ def process_df(data_frames_list: List[DataFrame]) -> bool:
     while search_params:
         for column, values in search_params.items():
             # fill in a new column `is_bourne` in case if we have a match with values from search_params
-            concatinated_df.loc[
-                concatinated_df[column].isin(values), "is_bourne"
+            concatenated_df.loc[
+                concatenated_df[column].isin(values), "is_bourne"
             ] = True
             # update used_params dict to skip searching by already used values
             used_params[column] = used_params.get(column, set()).union(values)
         # replace NaN with False
-        concatinated_df.is_bourne = concatinated_df.is_bourne.fillna(
-            False
-        )
+        concatenated_df.is_bourne = concatenated_df.is_bourne.fillna(False)
         # clear dict to restart cycle
         search_params.clear()
         # create new search dict {"<column_name>": ["<search_values>", ...]}
-        new_search_params: Dict[str, Any] = concatinated_df.loc[
-            concatinated_df.is_bourne
+        new_search_params: Dict[str, Any] = concatenated_df.loc[
+            concatenated_df.is_bourne
         ].to_dict("list")
         for column, values in new_search_params.items():
             # skip excluded columns
@@ -75,17 +68,19 @@ def process_df(data_frames_list: List[DataFrame]) -> bool:
             if search_set := set(
                 value
                 for value in set(values).difference(used_params.get(column, set()))
-                if value
+                if value is not None
             ):
                 # fill in search params with a new  key pairs (key -> column name, value -> column values)
                 search_params[column] = search_set
-    # ort dataframe by datetime
-    sorted_dataframe = concatinated_df.sort_values(by="Time")
-
-    for _, row in sorted_dataframe.loc[sorted_dataframe.is_bourne].iterrows():
-        should_print_jb = True
-        print(row["Time"], row["Full name"], row["file_name"])
-    return should_print_jb
+    # sort pd.DataFrame by datetime
+    filtered_df = concatenated_df.sort_values(by="Time").loc[concatenated_df.is_bourne]
+    result = [
+        f"{time} {name} {file_name}"
+        for time, name, file_name in zip(
+            filtered_df["Time"], filtered_df["Full name"], filtered_df["file_name"]
+        )
+    ]
+    return result
 
 
 if __name__ == "__main__":
--- reader.py
+++ reader.py
@@ -1,6 +1,5 @@
 import os
-from typing import Callable
-from typing import List
+from typing import Callable, List
 
 import pandas as pd
 
@@ -13,25 +12,23 @@ def read_file_to_df(file_path: str, *args, **kwargs) -> pd.DataFrame:
     """
     filename: str = os.path.basename(file_path)
     file_extension = filename.split(".")[-1]
-    if file_extension in [
+    if file_extension not in [
         "csv",
         "json",
     ]:
-        if file_extension == "csv":
-            kwargs["skipinitialspace"] = True
-        if file_extension == "json":  # assume we always have jsonl
-            kwargs["lines"] = True
-        pd_file_reader: Callable = getattr(pd, f"read_{file_extension}")
-        if kwargs["chunksize"]:
-            df_list: List[pd.DataFrame] = []
-            for chunk in pd_file_reader(file_path, *args, **kwargs):
-                df_list.append(chunk)
-            data_frame: pd.DataFrame = pd.concat(df_list)
-        else:
-            data_frame: pd.DataFrame = pd_file_reader(file_path, *args, **kwargs)
-        data_frame["file_name"] = filename
-    else:
         raise NotImplementedError(
             f"extension {file_extension} is not supported at the moment"
         )
+    if file_extension == "csv":
+        kwargs["skipinitialspace"] = True
+    if file_extension == "json":  # assume we always have jsonl
+        kwargs["lines"] = True
+    pd_file_reader: Callable = getattr(pd, f"read_{file_extension}")
+
+    df_list: List[pd.DataFrame] = []
+    for chunk in pd_file_reader(file_path, *args, **kwargs):
+        df_list.append(chunk)
+    data_frame: pd.DataFrame = pd.concat(df_list)
+    data_frame["file_name"] = filename
+
     return data_frame
