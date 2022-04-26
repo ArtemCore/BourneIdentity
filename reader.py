ed4f8189316812fd2733e71dfdf9cad18a17d7ad
Artem Tsitsvira <artem.t@core-tech.ru>
2022-04-26 14:14:48 +0300

    Added extra kwargs for read_ function based on file extension

--- reader.py
+++ reader.py
@@ -3,11 +3,12 @@ from typing import Callable
 
 import pandas as pd
 
+
 def read_file_to_df(file_path: str, *args, **kwargs) -> pd.DataFrame:
     """
-    simple file reader, try to determine file extension and read it
+    simple file reader, try to determine file extension and read it via pandas
     :param file_path:
-    :return:
+    :return: pd.DataFrame
     """
     filename: str = os.path.basename(file_path)
     file_extension = filename.split(".")[-1]
@@ -15,6 +16,10 @@ def read_file_to_df(file_path: str, *args, **kwargs) -> pd.DataFrame:
         "csv",
         "json",
     ]:
+        if file_extension == "csv":
+            kwargs["skipinitialspace"] = True
+        if file_extension == "json":  # assume we always have jsonl
+            kwargs["lines"] = True
         pd_file_reader: Callable = getattr(pd, f"read_{file_extension}")
         data_frame: pd.DataFrame = pd_file_reader(file_path, *args, **kwargs)
         data_frame["file_name"] = filename
