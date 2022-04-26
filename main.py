8f8011b243059a26aa0bd01feea840d5ea0b3cf2
Artem Tsitsvira <artem.t@core-tech.ru>
2022-04-26 15:51:14 +0300

    Added defauldict for used_params

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from typing import Any, Dict, List, Set
+from collections import defaultdict
+from typing import Any, DefaultDict, Dict, List, Set
 
 import numpy as np
 import pandas as pd
@@ -46,9 +47,9 @@ def process_df(data_frames_list: List[pd.DataFrame]) -> List[str]:
     """function to process list of pandas dataframes and search by params"""
     # concat all dataframes in one
     concatenated_df: pd.DataFrame = pd.concat(data_frames_list).replace({np.nan: None})
-    search_params: Dict[str, Set] = {"Full name": {"Jason Bourne",}}
+    search_params: Dict[str, Set] = {"Full name": {"Jason Bourne", }}
     exclude_columns: List[str] = ["Time", "is_bourne", "file_name"]
-    used_params: Dict[str, Set] = dict()
+    used_params: DefaultDict[str, Set] = defaultdict(set)
 
     while search_params:
         for column, values in search_params.items():
@@ -57,7 +58,7 @@ def process_df(data_frames_list: List[pd.DataFrame]) -> List[str]:
                 concatenated_df[column].isin(values), "is_bourne"
             ] = True
             # update used_params dict to skip searching by already used values
-            used_params[column] = used_params.get(column, set()).union(values)
+            used_params[column] = used_params[column].union(values)
         # replace NaN with False
         concatenated_df.is_bourne = concatenated_df.is_bourne.fillna(False)
         # clear dict to restart cycle
@@ -73,7 +74,7 @@ def process_df(data_frames_list: List[pd.DataFrame]) -> List[str]:
             # get search set (values minus already used values with None filter)
             if search_set := set(
                 value
-                for value in set(values).difference(used_params.get(column, set()))
+                for value in set(values).difference(used_params[column])
                 if value is not None
             ):
                 # fill in search params with a new  key pairs (key -> column name, value -> column values)
